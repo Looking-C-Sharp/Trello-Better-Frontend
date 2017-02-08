da0e3974cf751461a02b7adebb6d0e428e519921
aadvorsky <advorsky@iastate.edu>
2017-02-07 23:22:31 -0600

    login helper

---
+++ Sticky/Utils/LoginHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace Sticky.Utils
+{
+    class LoginHelper
+    {
+    //    public static async Task<bool> MicrosoftPassportAvailableCheckAsync()
+    //    {
+    //        bool keyCredentialAvailable = await KeyCredentialManager.IsSupportedAsync();
+    //        if (keyCredentialAvailable == false)
+    //        {
+    //            // Key credential is not enabled yet as user 
+    //            // needs to connect to a Microsoft Account and select a PIN in the connecting flow.
+    //            System.Diagnostics.Debug.WriteLine("Microsoft Passport is not setup!\nPlease go to Windows Settings and set up a PIN to use it.");
+    //            return false;
+    //        }
+
+    //        return true;
+    //    }
+    }
+}

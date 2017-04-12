546d83c5d16dd5bec4f88e1c78e3399fb1902893
Matthew Burket <m@tthewburket.com>
2017-04-11 19:44:11 -0500

    Stubbed User API methods

---
+++ Sticky/Controllers/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sticky.Controllers
+{
+    class User
+    {
+        public static bool SignIn(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool SignOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool SignUp(string username, string password, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
